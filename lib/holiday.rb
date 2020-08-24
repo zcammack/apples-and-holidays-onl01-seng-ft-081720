8278bb7847469efbbfaf7a13254ae9dd80133ff0
Zackary Cammack <zack.cammack@gmail.com>
2020-08-24 15:14:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,5 +67,9 @@ def all_supplies_in_holidays(holiday_hash)
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-holiday_hash.collect
+holiday_hash.collect do |season, holiday|
+  holiday.collect do |holiday, item|
+    holiday if item.include?("BBQ")
+    end
+  end.flatten.compact
 end
